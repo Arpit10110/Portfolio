30e8aa3fa2fcb0d776b8515ac6fa648494050014
Arpit <omagrahari55@gmail.com>
2023-05-28 03:11:53 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+//require
+const express=require("express");
+const path=require("path");
+const fs=require("fs");
+const port= process.env.PORT || 80;
+const app=express();
+//static
+// app.use('../static', express.static('static'));
+const static_path = path.join(__dirname, '../static');
+app.use(express.static(static_path));
+app.use(express.urlencoded());
+//views
+const home=fs.readFileSync("../view/index.html")
+const contact=fs.readFileSync('../view/contact.html');
+const service=fs.readFileSync('../view/service.html');
+//end points
+app.get('/', (req, res) => {
+    res.end(home);
+   })
+   app.get('/contact', (req, res) => {
+    res.end(contact);
+   })
+   app.get('/service', (req, res) => {
+    res.end(service);
+   })
+ //listening port
+ app.listen(port, () => {
+     console.log(`Server is running on port ${port}`)
+ })
---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+function scrollproject(){
+    let project= document.getElementById("project");
+         project.scrollIntoView({
+           block: 'start',
+           behavior: 'smooth',
+           inline: 'start'
+         });
+ }
+    function scrollabout(){
+    let about= document.getElementById("about");
+         about.scrollIntoView({
+           block: 'start',
+           behavior: 'smooth',
+           inline: 'start'
+         });
+        }
